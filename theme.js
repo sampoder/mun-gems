e9fa93ef23f8d2e4f26ca26432a192726e77db02
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-10-05 10:35:36 +0800

    Initial commit

---
+++ theme.js
@@ -0,0 +1,30 @@
+const white = '#fffceb'
+const black = '#11111f'
+const blue = '#2d5dd7'
+
+export default {
+  fonts: {
+    body: '"Sorts Mill Goudy", serif',
+  },
+  googleFont: 'https://fonts.googleapis.com/css?family=Sorts+Mill+Goudy',
+  colors: {
+    text: black,
+    background: white,
+    link: blue,
+  },
+  styles: {
+    root: {
+      textAlign: 'left',
+    },
+    img: {
+      textAlign: 'left',
+      width: 'fit'
+    },
+    Slide: {
+      display: 'block',
+      padding: '2em',
+      textAlign: 'left',
+      maxWidth: '1000px'
+    },
+  },
+}
